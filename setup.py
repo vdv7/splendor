c458862022fd808e17fdeb663f937b6456fc45f1
Terry Stewart <tcstewar@uwaterloo.ca>
2018-07-30 23:53:49 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='splendor',
+    packages=['splendor'],
+    version='0.0.1',
+    author='Terry Stewart',
+    description='Splendor, the card game',
+    author_email='terry.stewart@gmail.com',
+    url='https://github.com/tcstewar/splendor',
+    license='GPLv3',
+    )
